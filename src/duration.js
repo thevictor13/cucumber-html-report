d7d8fe962c62657678f2384be2bcde04e04a7a0b
Andrey Lipatkin <lipatkin@gmail.com>
2017-04-20 00:47:19 +0300

    Added support for cucumber-js 2.0 reports

--- src/duration.js
+++ src/duration.js
@@ -1,7 +1,7 @@
 // Converts the duration from nanoseconds to seconds and minutes
 
 /**
- * Rouds a number to the supplied decimals. Only makes sense for floats!
+ * Rounds a number to the supplied decimals. Only makes sense for floats!
  * @param decimals The maximum number of decimals expected.
  * @param number The number to round.
  * @returns {number} The rounded number. Always returns a float!
@@ -10,20 +10,20 @@ function round (decimals, number) {
   return Math.round(number * Math.pow(10, decimals)) / parseFloat(Math.pow(10, decimals))
 }
 
-function formatDurationInSeconds (duration) {
-  return round(2, duration / 1000000000) + ' s'
+function formatDurationInSeconds (duration, isCucumber2 = false) {
+  return isCucumber2 ? round(2, duration / 1000) + ' s' : round(2, duration / 1000000000) + ' s'
 }
 
-function formatDurationInMinutesAndSeconds (duration) {
-  return Math.trunc(duration / 60000000000) + ' m ' + round(2, (duration % 60000000000) / 1000000000) + ' s'
+function formatDurationInMinutesAndSeconds (duration, isCucumber2 = false) {
+  return isCucumber2 ? Math.trunc(duration / 60000) + ' m ' + round(2, (duration % 60000) / 1000) + ' s' : Math.trunc(duration / 60000000000) + ' m ' + round(2, (duration % 60000000000) / 1000000000) + ' s'
 }
 
-function isMinuteOrMore (duration) {
-  return duration && duration / 60000000000 >= 1
+function isMinuteOrMore (duration, isCucumber2 = false) {
+  return isCucumber2 ? duration && duration / 60000 >= 1 : duration && duration / 60000000000 >= 1
 }
 
-function isMinuteOrLess (duration) {
-  return duration && duration / 60000000000 < 1
+function isMinuteOrLess (duration, isCucumber2 = false) {
+  return isCucumber2 ? duration && duration / 60000 < 1 : duration && duration / 60000000000 < 1
 }
 
 module.exports = {

83ea59a626cf454f5b515038e25872190a3f155f
Peter Leinonen <peter@leinonen.se>
2018-04-03 17:31:58 +0200

    fix unit test

--- src/template.spec.js
+++ src/template.spec.js
@@ -2,11 +2,12 @@
 
 const Template = require('./template')
 const expect = require('chai').expect
+const path = require('path')
 
 describe('Template', () => {
   describe('getTemplatePartials', () => {
     it('should get the partials', () => {
-      const partials = Template.getTemplatePartials()
+      const partials = Template.getTemplatePartials(path.join(__dirname, '..', 'templates', 'partials'))
       const expectedPartials = [
         'browser_logs',
         'footer',
